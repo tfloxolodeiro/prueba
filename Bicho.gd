dd5090a62cb0e8faa533193b4bbbe0d69f6c4925
tfloxolodeiro <tfloxolodeiro@est.frba.utn.edu.ar>
2022-06-05 20:52:31 -0300

    prueba

--- Bicho.gd
+++ Bicho.gd
@@ -1,19 +1,35 @@
 extends KinematicBody2D
 
-export var speed = 200
+export var health = 3
+export var baseScale = Vector2(0.1,0.1)
+export var baseSpeed = 200
 var velocity = Vector2()
 var player
 
 func init(_player):
 	player = _player
+	resize()
 
 func direction_to_move():
 	var direction_to_move = player.position - position
 	return direction_to_move.normalized()
 
 func _physics_process(_delta):
-	var instant_velocity = direction_to_move() * speed
+	var instant_velocity = direction_to_move() * speed()
 	velocity = move_and_slide(instant_velocity)
 
+func resize():
+	scale = baseScale * health
+
+func speed(): 
+	if health != 0: #Una vez exploto por entrar aca con health 0 xd
+		return baseSpeed / health
+	else:
+		return 0
+
 func hit():
-	queue_free()
+	health -= 1
+	if health == 0:
+		queue_free()
+	else:
+		resize()
--- bullet.gd
+++ bullet.gd
@@ -1,20 +1,37 @@
 extends KinematicBody2D
-
+export var baseScaleFactor = 0.5
+var baseScale = Vector2(baseScaleFactor,baseScaleFactor)
 var speed = 750
 var velocity = Vector2()
 
 func init(pos, dir):
 	rotation = dir
 	position = pos
+	scale = baseScale
 	velocity = Vector2(speed, 0).rotated(rotation)
 
-func _physics_process(delta):
-	var collision = move_and_collide(velocity * delta)
+func collide(collision):
 	if collision:
 		velocity = velocity.bounce(collision.normal)
 		if collision.collider.has_method("hit"):
 			collision.collider.hit()
 			queue_free()
+
+func percentage_time_left():
+	return $LifeTimer.time_left / $LifeTimer.wait_time
+
+func process_life_time():
+	var time_left = $LifeTimer.time_left 
+	if time_left == 0:
+		queue_free()
+	else:
+		scale = baseScale * percentage_time_left() 
+	
+func _physics_process(delta):
+	var collision = move_and_collide(velocity * delta)
+	collide(collision)
+	process_life_time()
+
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
